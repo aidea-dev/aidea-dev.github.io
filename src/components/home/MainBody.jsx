85b53a5d85b69e166fb6fc803696084d91afa231
federicoromeo <federico.romeo.98@gmail.com>
2023-11-14 15:51:50 +0100

    Revert "Updates"
    This reverts commit 74c3764461ad9c8b9cf4d54a7138f114af4437f3.

---
+++ src/components/home/MainBody.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Typist from 'react-typist-component';
+import { Jumbotron } from "./migration";
+
+const MainBody = React.forwardRef(
+  ({ gradient, title, message, subtitle, icons }, ref) => {
+    return (
+      <Jumbotron
+        fluid
+        id="home"
+        style={{
+          background: `linear-gradient(136deg,${gradient})`,
+          backgroundSize: "1200% 1200%",
+        }}
+        className="title bg-transparent bgstyle text-dark min-vh-100 d-flex align-content-center align-items-center flex-wrap m-0"
+      >
+        <Container className="text-center">
+        <h1 ref={ref} className="display-1">
+            <span className="title-font">{title.trim()}</span>
+            <span className="subtitle-font">{subtitle.trim()}</span>
+      </h1>
+          <Typist>
+            <div className="lead typist">
+              {message}
+            </div>
+          </Typist>
+          <div className="p-5">
+            {icons.map((icon, index) => (
+              <a
+                key={`social-icon-${index}`}
+                target="_blank"
+                rel="noopener noreferrer"
+                href={icon.url}
+                aria-label={`My ${icon.image.split("-")[1]}`}
+              >
+                <i className={`fab ${icon.image}  fa-3x socialicons`} />
+                <i className={`fas ${icon.image}  fa-3x socialicons`} />
+              </a>
+            ))}
+          </div>
+          <a
+            className="btn btn-outline-dark btn-lg "
+            href="#aboutme"
+            role="button"
+            aria-label="Learn more about me"
+          >
+            More about us
+          </a>
+        </Container>
+      </Jumbotron>
+    );
+  }
+);
+
+export default MainBody;
